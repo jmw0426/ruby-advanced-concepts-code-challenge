b46f3e549bbe33b67b81af4eb779719bf31cea5d
jmw0426 <jmw0426@hotmail.com>
2020-09-02 16:04:29 -0500

    Initial Commit

--- cli.rb
+++ cli.rb
@@ -10,4 +10,26 @@ class CLI
             #2. exit program
     end
 
+    def welcome
+        puts "Welcome to NotZillow."
+        puts "We are looking for your new home."
+    end
+
+    def getdata
+        # API.new.tap { |api| api.fetch_json_data}
+        API.new.create_listings
+        # api = API.new
+        # api.fetch_json_data
+    end
+
+    def display_listings
+        Listing.all.each do |listing| 
+            puts "Location: #{listing.location}" 
+            puts "Status: #{listing.status}"
+            puts "Price: #{listing.price}" 
+            puts "Agent: #{listing.agent}"
+        end
+        # :location :status, :price, :agent
+    end
+
 end
